a2d2f05910ab366a487ee06a7d9a9964ceeee434
Popey Gilbert <popey456963@users.noreply.github.com>
2016-10-20 15:55:25 +0100

    Create a.js

---
+++ a.js
@@ -0,0 +1,5 @@
+var l = document.createElement('link');
+l.setAttribute("href", "https://codefined.github.io/a.css");
+l.setAttribute("rel", "stylesheet");
+l.setAttribute("type", "text/css");
+document.getElementsByTagName('head')[0].appendChild(l.childNodes[1]);
